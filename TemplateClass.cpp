06cf95bd64904c96a20f3641b0ffaa2c66095090
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-04-03 21:30:07 +0530

    Create TemplateClass.cpp

---
+++ TemplateClass.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+template<class T>
+class Stack{
+private:
+    T *stk;
+    int top;
+    int size;
+public:
+    Stack(int sz)
+    {
+        size = sz;
+        top = -1;
+        stk = new T[size];
+
+    }
+    void push(T x);
+    T pop();
+};
+
+template<class T>
+void Stack<T> :: push(T x)
+{
+    if(top == size-1)
+        cout<<"Stack is FULL:"<<endl;
+    else{
+        top++;
+        stk[top] = x;
+    }
+
+}
+template<class T>
+T Stack<T>:: pop()
+{
+    T x = 0;
+    if(top == -1)
+        cout<<"Stack underflow"<<endl;
+    else
+    {
+        x = stk[top];
+        top--;
+    }
+    return x;
+}
+int main()
+{
+    Stack<float> s(10);
+    s.push(10);
+    s.push(23);
+    s.push(90);
+
+}
