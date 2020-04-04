e41f673d79748a9ef19cf8599972c37883401fb9
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-04-04 14:07:02 +0530

    Create Namespace1.cpp

---
+++ Namespace1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+namespace First{
+void fun(}{
+         cout<<"First"<<endl;
+         }
+};
+namespace Second{
+    void fun(){
+        cout<<"Second"<<endl;
+    }
+};
+
+int main(){
+    First::fun();
+    Second::fun();
+}
